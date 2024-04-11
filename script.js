557c416e0bff27431d52c080d766ecc0e7d79fbd
benjamin.kurtisi <benjamin.kurtisi@tritum.de>
2024-04-11 10:06:24 +0200

    legend label and dynamic graph size

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const eduUrl =
 let width = document.getElementById("root").scrollWidth * 0.9;
 let height = window.innerHeight * 0.7;
 
+// General Document Structure
 const root = d3.select("#root");
-const header = root.append("div");
+root.style("height", "100vh");
+const header = root.append("div").attr("id", "header");
 const title = header
   .append("h1")
   .text("FreeCodeCamp Choropleth Map")
@@ -17,15 +19,17 @@ const description = header
   .text("US Educational Attainment")
   .attr("id", "description");
 
-root.append("div").attr("id", "graph");
-const svg = root
+const graph = root.append("div").attr("id", "graph");
+const svg = graph
   .append("svg")
   .attr("width", width)
   .attr("height", height)
+  .attr("viewBox", "0 0 960 600")
   .attr("id", "choropleth");
 
 const path = d3.geoPath();
 
+// API call(s)
 d3.json(countyUrl).then((countyData) => {
   const counties = topojson.feature(countyData, countyData.objects.counties);
   console.log(counties);
@@ -53,58 +57,62 @@ d3.json(countyUrl).then((countyData) => {
         if (result[0]) {
           return getColor(result[0].bachelorsOrHigher);
         }
-        return 'red';
+        return "red";
       });
   });
 });
 
-const legend = root.append('svg');
-legend.attr('width', width);
-legend.attr('height', 150);
-legend.attr('id', 'legend');
+// Legend
+graph.append("br");
+const legend = graph.append("svg");
+legend.attr("width", 650);
+legend.attr("height", 150);
+legend.attr("id", "legend");
 
-const legendScale = d3.scaleLinear()
-  .domain([0, 10])
-  .range([50, 550]);
+const legendScale = d3.scaleLinear().domain([0, 10]).range([50, 550]);
 
 legendAxis = d3.axisBottom(legendScale);
-legendAxis.tickFormat((d) => d + '%');
+legendAxis.tickFormat((d) => d + "%");
 
-legend.append('g')
-  .attr('transform', 'translate(0,100)')
-  .call(legendAxis);
+legend.append("g").attr("transform", "translate(0,100)").call(legendAxis);
 
 const colors = [];
 for (let i = 5; i <= 95; i += 10) {
   colors.push(getColor(i));
 }
 
-legend.selectAll('rect')
+legend
+  .selectAll("rect")
   .data(colors)
   .enter()
-  .append('rect')
-  .attr('height', 50)
-  .attr('width', 50)
-  .attr('x', (d, i) => legendScale(i))
-  .attr('y', 50)
-  .attr('fill', (d) => d);
+  .append("rect")
+  .attr("height", 50)
+  .attr("width", 50)
+  .attr("x", (d, i) => legendScale(i))
+  .attr("y", 50)
+  .attr("fill", (d) => d);
 
-const legendLabel = legend.select('g').append('text');
-legendLabel.attr('y', -70)
-  .attr('x', legendScale(colors.length / 2))
-  .attr('text-anchor', 'middle')
-  .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
+const legendLabel = legend.select("g").append("text");
+legendLabel
+  .attr("y", -70)
+  .attr("x", legendScale(colors.length / 2))
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .attr("font-size", ".8rem")
+  .text(
+    "Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)"
+  );
 
 function getColor(eduRate) {
-  if(eduRate > 90) return '#1864ab';
-  if(eduRate > 80) return '#1971c2';
-  if(eduRate > 70) return '#1c7ed6';
-  if(eduRate > 60) return '#228be6';
-  if(eduRate > 50) return '#339af0';
-  if(eduRate > 50) return '#4dabf7';
-  if(eduRate > 40) return '#74c0fc';
-  if(eduRate > 30) return '#a5d8ff';
-  if(eduRate > 20) return '#d0ebff';
-  if(eduRate >= 10) return '#e7f5ff';
-  return '#f03e3e';
+  if (eduRate > 90) return "#1864ab";
+  if (eduRate > 80) return "#1971c2";
+  if (eduRate > 70) return "#1c7ed6";
+  if (eduRate > 60) return "#228be6";
+  if (eduRate > 50) return "#339af0";
+  if (eduRate > 50) return "#4dabf7";
+  if (eduRate > 40) return "#74c0fc";
+  if (eduRate > 30) return "#a5d8ff";
+  if (eduRate > 20) return "#d0ebff";
+  if (eduRate >= 10) return "#e7f5ff";
+  return "#f03e3e";
 }
